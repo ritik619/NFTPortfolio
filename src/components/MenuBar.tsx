dbcb400c76eb393aa9f6687e477cdf2941a35c7e
ritik619 <ritiksaini619@gmail.com>
2022-06-25 02:29:28 +0530

    worked on menuBar

--- src/components/MenuBar.tsx
+++ src/components/MenuBar.tsx
@@ -1,7 +1,8 @@
-import { Card, Stack, Typography } from "@mui/material";
+import { Card, Divider, IconButton, Stack, Typography } from "@mui/material";
 import Image from "next/image";
 import React from "react";
-
+import ArrowDropDownIcon from "@mui/icons-material/ArrowDropDown";
+import { Icon } from "@iconify/react";
 type Props = {};
 
 const MenuBar = (props: Props) => {
@@ -10,12 +11,37 @@ const MenuBar = (props: Props) => {
       <Card
         sx={{
           backgroundColor: "#f0efe0",
-          height: "25px",
+          height: "24px",
           border: "2px solid #fffbef",
           borderRadius: "0px",
         }}
       >
-        <Stack ml={2} spacing={2} direction="row">
+        <Stack direction="row" justifyContent="space-between">
+          <Stack ml={2} spacing={2} direction="row">
+            <Typography>File</Typography>
+            <Typography>Edit</Typography>
+            <Typography>View</Typography>
+            <Typography>Favourites</Typography>
+            <Typography>Tools</Typography>
+            <Typography>Help</Typography>
+          </Stack>
+          <Image
+            src="/windows-logo.jpg"
+            alt="/windows-logo.jpg"
+            width="24px"
+            height="24px"
+          />
+        </Stack>
+      </Card>
+      <Card
+        sx={{
+          backgroundColor: "#f0efe0",
+          height: "38px",
+          border: "2px solid #fffbef",
+          borderRadius: "0px",
+        }}
+      >
+        <Stack m={1} spacing={2} direction="row">
           <Image
             alt="/XP/24x24/actions/back.png"
             src="/XP/24x24/actions/back.png"
@@ -23,17 +49,27 @@ const MenuBar = (props: Props) => {
             width="24px"
           />
           <Typography>Back</Typography>
+          <ArrowDropDownIcon sx={{ marginLeft: "0!important" }} />
           <Image
             src="/XP/24x24/actions/forward.png"
             alt="/XP/24x24/actions/forward.png"
             height="24px"
             width="24px"
           />
+          <ArrowDropDownIcon sx={{ marginLeft: "0!important" }} />
           <Image
             alt="/XP/24x24/places/stock_folder.png"
             src="/XP/24x24/actions/fileopen.png"
             height="24px"
             width="24px"
+          />
+          <Divider
+            orientation="vertical"
+            flexItem
+            sx={{
+              color: "#8a8b82",
+              mr: "10px !important",
+            }}
           />
           <Image
             alt="/XP/128x128/apps/search.png"
@@ -49,6 +85,15 @@ const MenuBar = (props: Props) => {
             width="24px"
           />
           <Typography>Folders</Typography>
+          <Divider
+            orientation="vertical"
+            flexItem
+            color="#8a8b82"
+            sx={{
+              color: "#8a8b82",
+              mr: "10px !important",
+            }}
+          />
           <Image
             alt="/XP/24x24/actions/back.png"
             src="/XP/24x24/categories/stock_internet.png"
@@ -60,18 +105,33 @@ const MenuBar = (props: Props) => {
       <Card
         sx={{
           backgroundColor: "#f0efe0",
-          height: "25px",
+          height: "24px",
           border: "2px solid #fffbef",
           borderRadius: "0px",
         }}
       >
-        <Stack ml={2} spacing={2} direction="row">
-          <Typography>File</Typography>
-          <Typography>Edit</Typography>
-          <Typography>View</Typography>
-          <Typography>Favourites</Typography>
-          <Typography>Tools</Typography>
-          <Typography>Help</Typography>
+        <Stack direction="row" spacing={2} justifyContent="space-between">
+          <Typography>Address</Typography>
+          <Stack
+            sx={{
+              width: "100%",
+              backgroundColor: "white",
+              border: "2px solid #fffbef",
+            }}
+          >
+            <Typography>
+              C:\Documents and Settings\111\Local Settings\Temp
+            </Typography>
+            <Icon icon="ri:arrow-drop-down-line" style={{backgroundColor:'blue'}}/>
+          </Stack>
+          <Card>
+            <Image
+              src="/XP/24x24/actions/go-next-symbolic.png"
+              alt="/XP/24x24/actions/go-next-symbolic.png"
+              width="24px"
+              height="24px"
+            />
+          </Card>
         </Stack>
       </Card>
     </Stack>
